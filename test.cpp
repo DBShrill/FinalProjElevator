93685b723264cd9c3e07c1f85f0f2d12216de475
zihanl2003 <zihanl@umich.edu>
2025-04-04 16:07:41 -0400

    test person & elevator (also debug person.cpp)

--- test.cpp
+++ test.cpp
@@ -34,10 +34,14 @@ using namespace std;
 void testFloorAddPerson();
 void start_tests();
 void testFloorRemovePerson();
+void testPerson();
+void testElevator();
 
 //temporary main function
 int main() {
     start_tests();
+    testPerson();
+    testElevator();
 }
 
 // declare your test functions here
@@ -75,4 +79,83 @@ void testFloorRemovePerson() {
     floor.removePeople(remove, 3);
     cout << "Testing Floor Remove Person\n";
     cout << "ExpectingL 3f0t9a1, Actual: " << floor.getPersonByIndex(0) << endl;
+}
+
+void testPerson() {
+    cout << "Testing Person class\n";
+
+    // Test constructor and getters
+    Person p("7f4t8a3");
+    cout << "Expecting turn: 7, Actual: " << p.getTurn() << endl;
+    cout << "Expecting currentFloor: 4, Actual: " << p.getCurrentFloor() << endl;
+    cout << "Expecting targetFloor: 8, Actual: " << p.getTargetFloor() << endl;
+    cout << "Expecting angerLevel: 3, Actual: " << p.getAngerLevel() << endl;
+
+    // Test tick - not increasing anger if not on tick interval
+    cout << "Testing tick at time 1 (no anger increase expected)" << endl;
+    p.tick(1);
+    cout << "Expecting angerLevel: 3, Actual: " << p.getAngerLevel() << endl;
+
+    // Test tick - increase anger at TICKS_PER_ANGER_INCREASE
+    cout << "Testing tick at time 5 (anger should increase by 1)" << endl;
+    bool result = p.tick(5);
+    cout << "Expecting angerLevel: 4, Actual: " << p.getAngerLevel() << endl;
+    cout << "Expecting result: false, Actual: " << result << endl;
+
+    // Keep ticking until MAX_ANGER - 1 is reached
+    while (p.getAngerLevel() < MAX_ANGER - 1) {
+        result = p.tick(p.getAngerLevel() * TICKS_PER_ANGER_INCREASE);
+    }
+    cout << "Testing tick when reaching MAX_ANGER - 1" << endl;
+    cout << "Expecting angerLevel: " << MAX_ANGER - 1 << ", Actual: " << p.getAngerLevel() << endl;
+    cout << "Expecting result: true, Actual: " << result << endl;
+
+    // Test print
+    cout << "Testing print\n";
+    cout << "Expecting: f4t8a" << p.getAngerLevel() << ", Actual: ";
+    p.print(cout);
+    cout << endl;
+
+    // Test operator<< overload
+    cout << "Testing << operator overload\n";
+    cout << "Expecting: f4t8a" << p.getAngerLevel() << ", Actual: " << p << endl;
+}
+
+void testElevator() {
+    cout << "Testing Elevator class\n";
+
+    Elevator e;
+
+    // Test initial state
+    cout << "Expecting currentFloor: 0, Actual: " << e.getCurrentFloor() << endl;
+    cout << "Expecting isServicing: 0, Actual: " << e.isServicing() << endl;
+
+    // Test serviceRequest
+    e.serviceRequest(5);
+    cout << "After serviceRequest(5)\n";
+    cout << "Expecting targetFloor: 5, Actual: " << e.getTargetFloor() << endl;
+    cout << "Expecting isServicing: 1, Actual: " << e.isServicing() << endl;
+
+    // Test tick - simulate multiple ticks until reaching target
+    for (int time = 0; e.isServicing(); ++time) {
+        e.tick(time * TICKS_PER_ELEVATOR_MOVE);
+        cout << "Tick time " << time * TICKS_PER_ELEVATOR_MOVE << ", Floor: " << e.getCurrentFloor() << endl;
+    }
+    cout << "Expecting final floor: 5, Actual: " << e.getCurrentFloor() << endl;
+    cout << "Expecting isServicing: 0, Actual: " << e.isServicing() << endl;
+
+    // Test going downward
+    e.serviceRequest(2);
+    for (int time = 0; e.isServicing(); ++time) {
+        e.tick(time * TICKS_PER_ELEVATOR_MOVE);
+        cout << "Tick time " << time * TICKS_PER_ELEVATOR_MOVE << ", Floor: " << e.getCurrentFloor() << endl;
+    }
+    cout << "Expecting final floor: 2, Actual: " << e.getCurrentFloor() << endl;
+    cout << "Expecting isServicing: 0, Actual: " << e.isServicing() << endl;
+
+    // Test print
+    cout << "Testing print()\n";
+    cout << "Expecting: 2w, Actual: ";
+    e.print(cout);
+    cout << endl;
 }

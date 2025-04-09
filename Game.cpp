a4e9c2e04a87ebfed1feb609fc3869a2d98987d8
spencerhu123 <1698neilave@gmail.com>
2025-04-09 18:58:58 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -80,24 +80,64 @@ void Game::playGame(bool isAIModeIn, ifstream& gameFile) {
 bool Game::isValidPickupList(const string& pickupList, 
                              const int pickupFloorNum) const {
    
-            for ( int i= 0; i < pickupList.size()-1;i++ ){
-                   if (pickupList.at(i) == pickupList.at(i+1)){
-                    return false;
-
+               // 1. Check for duplicate indices
+            for (int i = 0; i < pickupList.size()-1; i++) {
+                    for (int j = i+1; j < pickupList.size(); j++) {
+                        if (pickupList.at(i) == pickupList.at(j)) {
+                return false;
             }
+        }
     }
 
+            // 2. Check each character is a digit between '0'-'9'
             for (int k =0; k < pickupList.size(); k++){
                 if (!isdigit(pickupList.at(i))){
+
                     return false;
                 }
+            }
 
+            ////3. not sure,The length of the pickupList is less than or equal to the capacity of an elevator
+           const int max_capability = 10;
+            if (pickupList.size() > max_capability){
+                return false;
 
             }
 
+            // Get the floor and its people count
+            Floor pickupFloor = Building::getFloorByFloorNum(pickupFloorNum);
+            int numPeople = pickupFloor.getNumPeople();
 
+            // 4. Check all indices are valid (less than number of people on floor)
+            for (int m = 0; m < pickupList.size(); m++) {
+             int index = pickupList.at(m) - '0'; // Convert char to int
+                if (index >= numPeople) {
+                    return false;
+        }
+    }
+        // 5. Check all selected people are going in same direction
+        if (pickupList.size() > 0) {
+            // Get first person's direction as reference
+            int firstIndex = pickupList.at(0) - '0';
+            Person firstPerson = pickupFloor.getPersonByIndex(firstIndex);
+            int firstTarget = firstPerson.getTargetFloor();
+            bool firstDirection = (firstTarget > pickupFloorNum); // true = up, false = down
+
+            for (int n = 1; n < pickupList.size(); n++) {
+                int currentIndex = pickupList.at(n) - '0';
+                Person currentPerson = pickupFloor.getPersonByIndex(currentIndex);
+                int currentTarget = currentPerson.getTargetFloor();
+                bool currentDirection = (currentTarget > pickupFloorNum);
+
+                if (currentDirection != firstDirection) {
+                    return false;
+                }
+            }
+        }
+        
     return true;
 }
+
 
 //////////////////////////////////////////////////////
 ////// DO NOT MODIFY ANY CODE BENEATH THIS LINE //////

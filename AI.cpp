d24602e3eb21d53752d0c46ab6728dcaf552f243
DBShrill <flamingfireball361@gmail.com>
2025-04-17 20:50:51 -0400

    untested getAIMoveString

--- AI.cpp
+++ AI.cpp
@@ -19,10 +19,52 @@
 // You do not need to make any changes to this file for the Core
 
 string getAIMoveString(const BuildingState& buildingState) {
-    return "";
+    int totalPeople = 0;
+
+    for (int i = 0; i < NUM_FLOORS; i++) {
+        totalPeople += buildingState.floors[i].numPeople;
+    }
+
+    if (totalPeople == 0) {
+        return "";
+    }
+
+    int servicingCount = 0;
+    for (int i = 0; i < NUM_ELEVATORS; i++) {
+        if (buildingState.elevators[i].isServicing) {
+            servicingCount++;
+        }
+    }
+
+    if (servicingCount == NUM_ELEVATORS) {
+        return "";
+    }
+
+    for (int i = 0; i < NUM_ELEVATORS; i++) {
+        if (!buildingState.elevators[i].isServicing) {
+            int currentFloor = buildingState.elevators[i].currentFloor;
+            int peopleOnCurrentFloor = buildingState.floors[currentFloor].numPeople;
+
+            if (peopleOnCurrentFloor > 0) {
+                return "e" + to_string(i) + "p";
+            }
+
+            else {
+                int maxPeople = 0;
+                int targetFloor = 0;
+                for (int j = 0; j < NUM_FLOORS; j++) {
+                    if (buildingState.floors[j].numPeople > maxPeople) {
+                        maxPeople = buildingState.floors[j].numPeople;
+                        targetFloor = j;
+                    }
+                }
+                return "e" + to_string(i) + "f" + to_string(targetFloor);
+            }
+        }
+    }
 }
 
-string getAIPickupList(const Move& move, const BuildingState& buildingState, 
+string getAIPickupList(const Move& move, const BuildingState& buildingState,
                        const Floor& floorToPickup) {
     return "";
 }
